bc818a84ab889bc23511004fccc9e00c3f5ad14a
Jacob Hummer <jcbhmr@outlook.com>
2023-01-26 02:27:59 +0000

    Add Water.css

--- docs/pages/index.php
+++ docs/pages/index.php
@@ -24,6 +24,7 @@ $ids = array_diff(scandir($srcDir), ['.', '..']);
   <title>Document</title>
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
 </head>
 
 <body>
