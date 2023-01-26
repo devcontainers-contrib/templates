9ba9114aa5bc2673b511fac1c6e8cbb6704f0d83
Jacob Hummer <jcbhmr@outlook.com>
2023-01-26 02:30:03 +0000

    Set title

--- docs/pages/[id].php
+++ docs/pages/[id].php
@@ -22,7 +22,9 @@ extract($devcontainerTemplate);
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-  <title>Document</title>
+  <title>
+    <?= $name ?>
+  </title>
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
